da6cb5e6cdb933b8c9f89b1105f3d619082de447
Guilherme Feuser <guiguifeuser@gmail.com>
2021-06-28 18:34:39 -0300

    Update Constants

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/music/YoutubeMusicConstants.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/music/YoutubeMusicConstants.java
@@ -8,7 +8,7 @@ public class YoutubeMusicConstants {
     static final String MUSIC_CLIENT_VERSION = "1.20210524.00.00";
     static final String MUSIC_BASE_PAYLOAD = "{\"context\":{\"client\":{\"clientName\":\"" + MUSIC_CLIENT_NAME + "\",\"clientVersion\":\"" + MUSIC_CLIENT_VERSION + "\"}},";
 
-    static final String MUSIC_SEARCH_URL = MUSIC_BASE_URL + "/search?key=";
+    static final String MUSIC_SEARCH_URL = MUSIC_BASE_URL + "/search?key=" + MUSIC_INNERTUBE_API_KEY;
     static final String MUSIC_SEARCH_PAYLOAD = MUSIC_BASE_PAYLOAD + "\"query\":\"%s\",\"params\":\"Eg-KAQwIARAAGAAgACgAMABqChADEAQQCRAFEAo=\"}";
 
     static final String MUSIC_WATCH_URL_PREFIX = MUSIC_ORIGIN + "/watch?v=";
