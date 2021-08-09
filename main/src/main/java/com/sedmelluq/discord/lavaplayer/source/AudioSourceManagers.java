e06bf0e14b2c7ee96e57e9b9202729c2b489938a
Guilherme Feuser <guiguifeuser@gmail.com>
2021-08-08 21:22:33 -0300

    Smule support

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/AudioSourceManagers.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/AudioSourceManagers.java
@@ -13,6 +13,7 @@ import com.sedmelluq.discord.lavaplayer.source.tiktok.TikTokAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.mixcloud.MixcloudAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.bandlab.BandlabAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.saavn.SaavnAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.smule.SmuleAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.jamendo.JamendoAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.yamusic.YandexMusicAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
@@ -41,6 +42,7 @@ public class AudioSourceManagers {
     playerManager.registerSourceManager(new MixcloudAudioSourceManager(true));
     playerManager.registerSourceManager(new JamendoAudioSourceManager(true));
     playerManager.registerSourceManager(new SaavnAudioSourceManager(true));
+    playerManager.registerSourceManager(new SmuleAudioSourceManager());
     playerManager.registerSourceManager(SoundCloudAudioSourceManager.createDefault());
     playerManager.registerSourceManager(new BandlabAudioSourceManager());
     playerManager.registerSourceManager(new TikTokAudioSourceManager());
