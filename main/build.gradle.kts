63e14ce5d7fe4be36824e585220ab73d4d373c43
WearifulCupid0 <guiguifeuser@gmail.com>
2022-04-24 21:00:42 -0300

    davidffa natives

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -9,7 +9,7 @@ version = "1.3.99"
 
 dependencies {
   api("com.sedmelluq:lava-common:1.1.2")
-  implementation("com.github.walkyst:lavaplayer-natives-fork:1.0.0")
+  implementation("com.github.davidffa:lavaplayer-natives-fork:1.0.0")
   implementation("com.github.walkyst.JAADec-fork:jaadec-ext-aac:0.1.3")
   implementation("org.mozilla:rhino-engine:1.7.14")
   implementation("org.slf4j:slf4j-api:1.7.25")
